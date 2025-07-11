6f65d6ad7ae1f2335e7849a90335161824a43971
endeav007 <autumnaustin046@gmail.com>
2025-07-11 10:24:24 -0700

    finalized styling

---
+++ script.js
@@ -0,0 +1,124 @@
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+const submit = document.getElementById("submit-button")
+
+
+
+submit.addEventListener("click", () => {
+
+
+    //user input
+    let inputDay = document.forms["myForm"]["fday"].value;
+    let inputMonth = document.forms["myForm"]["fmonth"].value;
+    let inputYear = document.forms["myForm"]["fyear"].value;
+    
+    //validation checks
+    let validDay = false;
+    let validMonth = false;
+    let validYear = false;
+
+    //resetting after input
+    document.getElementById("day-error").innerHTML = "";
+    document.getElementById("month-error").innerHTML = "";
+    document.getElementById("year-error").innerHTML = "";
+
+
+    //current date
+    const date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getUTCFullYear();
+
+    //error messages
+    let errorEmpty = "This field is required";
+    let errorMonth = "Must be a valid month";
+    let errorDay = "Must be a valid date";
+    let errorYear = "Must be a year in the past";
+
+
+    // Year Validation
+    if(inputYear == ""){
+        //display error message -> empty
+        document.getElementById("year-error").innerHTML = errorEmpty;
+        document.getElementById("year").style.borderColor = "red";
+        document.getElementById("year-error").style.color = "red";
+        validYear = false;
+
+    }
+    else if(inputYear > year || inputYear < 1){
+        //invalid year
+        validYear = false;
+        document.getElementById("year-error").innerHTML = errorYear;
+        document.getElementById("year").style.borderColor = "red";
+        document.getElementById("year-error").style.color = "red";
+    }
+    else{
+        validYear = true;
+        document.getElementById("year").style.borderColor = "";
+        document.getElementById("year-error").style.color = "";
+
+    }
+
+    // Month Validation
+    if(inputMonth == "")
+    {
+        //display error message
+        document.getElementById("month-error").innerHTML = errorEmpty;
+        document.getElementById("month").style.borderColor = "red";
+        document.getElementById("month-error").style.color = "red";
+        validMonth = false;
+    }
+    else if(inputMonth < 1 || inputMonth > 12)
+    {
+        //invalid moth
+        document.getElementById("month-error").innerHTML = errorMonth;
+         document.getElementById("month").style.borderColor = "red";
+        document.getElementById("month-error").style.color = "red";
+        validMonth = false;
+    }
+    else{
+        validMonth = true;
+        document.getElementById("month").style.borderColor = "";
+        document.getElementById("month-error").style.color = "";
+        
+    }
+
+    //Day Validation
+    if(inputDay == "")
+    {
+        //display error message
+        document.getElementById("day-error").innerHTML = errorEmpty;
+        document.getElementById("day").style.borderColor = "red";
+        document.getElementById("day-error").style.color = "red";
+        validDay = false;
+    }
+    else if(inputDay < 1 || inputDay > 31)
+    {
+        //invalid days
+        document.getElementById("day-error").innerHTML = errorDay;
+        document.getElementById("day").style.borderColor = "red";
+        document.getElementById("day-error").style.color = "red";
+        validDay = false;
+    }
+    else{
+         document.getElementById("day").style.borderColor = "";
+        document.getElementById("day-error").style.color = "";
+        validDay = true;
+    }
+
+    if(validYear == true && validDay == true && validMonth == true)
+    {
+        document.getElementById("result-years").innerHTML = year - inputYear;
+        document.getElementById("result-months").innerHTML = Math.abs(month - inputMonth);
+        document.getElementById("result-days").innerHTML = Math.abs(day - inputDay);
+
+    }
+    
+
+
+    return false;
+
+});
+
+
